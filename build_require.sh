18e1cde2c7763ca66b1934e8fcd86fb5729fd610
mpskex <mpskex@163.com>
2017-07-21 21:13:11 +0800

    Push

--- build_require.sh
+++ build_require.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
+#-----------------------------------------------------------
+#	This is a tool to compile all the 
+#	package that is needed in GRiNa
+#	mpsk
+#	2017/07/21
+#-----------------------------------------------------------
 LUA_ENABLED="false"
 CLEAN="false"
 BUILD_EXT="true"
-
+for i in `find . -name "*.log"`; do rm $i; done
 LOG_PATH="$PWD/make.log"
-echo "++LOG++" > LOG_PATH
+echo "++LOG++" > $LOG_PATH
 
 WORK_PATH="$PWD/required"
 echo $WORK_PATH
 
 if [ $LUA_ENABLED = "true" ]; then
+	echo "Lua Proccessing.."
     cd $WORK_PATH/lua
-
     make clean  >> $LOG_PATH
     if [$CLEAN="false"]; then 
         make -j4 all   >> $LOG_PATH
+		sleep 2s
     fi
     sudo make install  >> $LOG_PATH
+	echo "Lua Compiled"
 fi
 
 cd $WORK_PATH/SDL2
+echo "SDL2 Proccessing.."
 sh configure  >> $LOG_PATH
 make clean  >> $LOG_PATH
 if [ $CLEAN = "false" ]; then 
 	make -j4 all  >> $LOG_PATH
+	sleep 2s
+	sudo make install  >> $LOG_PATH
+	echo "SDL2 Compiled"
 fi
-sudo make install  >> $LOG_PATH
 
+echo "SDL2_image Proccessing.."
 if [ $BUILD_EXT = "true" ]; then
     #   build libjpeg-dev
     cd $WORK_PATH/SDL2_image/external/jpeg-9
@@ -34,39 +46,45 @@ if [ $BUILD_EXT = "true" ]; then
 	make clean  >> $LOG_PATH
 	if [ $CLEAN = "false" ]; then 
 		make -j4 all  >> $LOG_PATH
+		sleep 2s
+		sudo make install  >> $LOG_PATH
 	fi
-	sudo make install  >> $LOG_PATH
     #   build libpng-dev
     cd $WORK_PATH/SDL2_image/external/libpng-1.6.2
 	sh configure  >> $LOG_PATH
 	make clean  >> $LOG_PATH
 	if [ $CLEAN = "false" ]; then 
 		make -j4 all  >> $LOG_PATH
+		sleep 2s
+		sudo make install  >> $LOG_PATH
 	fi
-	sudo make install  >> $LOG_PATH
     #   build zlib
 	cd $WORK_PATH/SDL2_image/external/zlib-1.2.8
 	sh configure  >> $LOG_PATH
 	make clean  >> $LOG_PATH
 	if [ $CLEAN = "false" ]; then 
 		make -j4 all  >> $LOG_PATH
+		sleep 2s
+		sudo make install  >> $LOG_PATH
 	fi
-	sudo make install  >> $LOG_PATH
     #   build libwebp-0.3.0
     cd $WORK_PATH/SDL2_image/external/libwebp-0.3.0
 	sh configure  >> $LOG_PATH
 	make clean  >> $LOG_PATH
 	if [ $CLEAN = "false" ]; then 
 		make -j4 all  >> $LOG_PATH
+		sleep 2s
+		sudo make install  >> $LOG_PATH
 	fi
-	sudo make install  >> $LOG_PATH
 fi
 cd $WORK_PATH/SDL2_image
 sh configure  >> $LOG_PATH
-make clean
+make clean  >> $LOG_PATH
 if [ $CLEAN = "false" ]; then 
 	make -j4 all  >> $LOG_PATH
+	sleep 2s
+	sudo make install  >> $LOG_PATH
+	echo "SDL2 Compiled"
 fi
-sudo make install  >> $LOG_PATH
 
 exit 0
