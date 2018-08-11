5b3e1e069230a52efadfde026d94a838edcfc8bc
mpsk <mpskex@163.com>
2018-08-11 20:17:53 +0800

    update windows configuration

--- Engine_private.h
+++ Engine_private.h
@@ -5,13 +5,13 @@
 #define ENGINE_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.3.2.42"
+#define VER_STRING	"0.3.2.43"
 #define VER_MAJOR	0
 #define VER_MINOR	3
 #define VER_RELEASE	2
-#define VER_BUILD	42
+#define VER_BUILD	43
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.3.2.42"
+#define FILE_VERSION	"0.3.2.43"
 #define FILE_DESCRIPTION	""
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
